4168aea087c85b6a87c89c4be0d7ac6a5e947680
grumpycatyo-collab <linkolnmax24@gmail.com>
2024-11-24 17:12:51 +0200

    Add structure for lab_3

---
+++ lab_3/domain/manufacturing_system.py
@@ -0,0 +1,23 @@
+class ManufacturingSystem:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ManufacturingSystem, cls).__new__(cls)
+            cls._instance.active_orders = []
+            cls._instance.completed_orders = []
+        return cls._instance
+
+    def add_order(self, order):
+        self.active_orders.append(order)
+
+    def complete_order(self, order):
+        if order in self.active_orders:
+            self.active_orders.remove(order)
+            self.completed_orders.append(order)
+
+    def get_active_orders(self):
+        return self.active_orders
+
+    def get_completed_orders(self):
+        return self.completed_orders
--- lab_3/models/donut.py
+++ lab_3/models/donut.py
@@ -6,12 +6,19 @@ class Donut:
         self.filling = None
         self.size = None
         self.ingredients = []
+        self.base_cost = 2.0
 
     def add_ingredient(self, ingredient):
         self.ingredients.append(ingredient)
 
     def clone(self):
         return deepcopy(self)
+
+    def get_cost(self):
+        return self.base_cost
+
+    def get_description(self):
+        return f"{self.size} donut with {self.filling} filling"
 
     def __str__(self):
         ingredients_str = ", ".join([i.get_name() for i in self.ingredients])
